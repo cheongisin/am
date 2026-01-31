6b05eb27b57f164d3ab747904a6abe97b18c4abb
cheongisin <hsk55661531@gmail.com>
2026-01-31 16:18:41 +0900

    Update gasApi.js

--- js/gasApi.js
+++ js/gasApi.js
@@ -3,7 +3,7 @@
 
 // ✅ 너가 배포한 GAS Web App URL을 넣어줘
 // 예: https://script.google.com/macros/s/XXXX/exec
-export const GAS_URL = window.https://script.google.com/macros/s/AKfycbwd2haaD4efa1cgHBOhD9oW7KjUYgUtl4yj0ytupTWEi3nxrGjSKi4iluryKj34cQ_7aQ/exec || '';
+export const GAS_URL = https://script.google.com/macros/s/AKfycbwd2haaD4efa1cgHBOhD9oW7KjUYgUtl4yj0ytupTWEi3nxrGjSKi4iluryKj34cQ_7aQ/exec || '';
 
 function mustHaveUrl(){
   if(!GAS_URL){
