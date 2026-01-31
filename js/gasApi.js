12dc2b7f0dc1e2a0aef3c7e4aec1178767e5f082
cheongisin <hsk55661531@gmail.com>
2026-02-01 00:34:33 +0900

    gasApi.js 업데이트

--- js/gasApi.js
+++ js/gasApi.js
@@ -2,7 +2,7 @@
 // - Netlify 기본: /.netlify/functions/state
 // - GAS 사용 시: 아래 GAS_URL에 Web App URL 넣기
 
-export const GAS_URL = ""; 
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbyvbIm7QytKeNYV0c2iP5RKO6rgUSe1IhG4RXlgmNcICMKKska_NjwLF7rcF-YJvMTZXg/exec";
 // 예) "https://script.google.com/macros/s/XXXX/exec"
 // Netlify만 쓸 거면 빈 문자열로 둬도 됨.
 
